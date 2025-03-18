368448f400437b0074ee188ef1255b1b53df82aa
ZmFkbHkK <fadliwaykanan@gmail.com>
2025-03-19 00:22:56 +0700

    Update install.py

--- noobzvpns/install.py
+++ noobzvpns/install.py
@@ -4,16 +4,19 @@ import time
 import requests
 
 # =====================================================
-# description: Installer noobzvpns with python3 by xdxl
+# Installer noobzvpns with python3 by xdxl
 # =====================================================
 
-os.system('clear')
+#os.system('clear')
 
-print(f"[ {time.ctime()} ] Processed Install noobzvpns service")
+#print(f"[ {time.ctime()} ] Processed Install noobzvpns service")
 time.sleep(2)
 
 os.makedirs('/etc/noobzvpns', exist_ok=True)
 open('/etc/noobzvpns/users.json', 'a').close()
+
+if os.path.isfile('/usr/bin/noobzvpns'):
+    os.remove('/usr/bin/noobzvpns')
 
 noobzvpns_url = "https://raw.githubusercontent.com/ZmFkbHkK/xd/main/noobzvpns/noobzvpns.x86-64"
 response = requests.get(noobzvpns_url)
@@ -35,4 +38,4 @@ subprocess.run(['systemctl', 'daemon-reload'])
 subprocess.run(['systemctl', 'enable', 'noobzvpns'])
 subprocess.run(['systemctl', 'restart', 'noobzvpns'])
 
-print(f"[ {time.ctime()} ] Install noobzvpns service Done")
+#print(f"[ {time.ctime()} ] Install noobzvpns service Done")
